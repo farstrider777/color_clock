5554607e28c4a5ff8e0f1248f2f45982544187d5
Joshua Bradford <jjyet@yahoo.com>
2017-02-06 15:13:23 -0500

    basic clock working, colors coming soon

---
+++ main.js
@@ -0,0 +1,29 @@
+
+jDiv = document.querySelector("div");
+var fnark = 0
+
+function trimDate(){
+  spacesInDate = 0;
+  onlyTime = [];
+  for(var count = 0; count < Date().length; count++){
+    if(Date()[count] === " "){
+      spacesInDate++;
+    }
+    if(spacesInDate === 4){
+      onlyTime.push(Date()[count]);
+    }
+  }
+  jDiv.innerHTML = onlyTime.join("");
+
+  fnark = onlyTime.filter(function (x){return x !== ":" && x !== " "}).join("");
+
+  jDiv.style.backgroundColor = fnark;
+}
+
+trimDate();
+
+
+
+setInterval(trimDate, 1000);
+
+//div.style.marginTop = ".25in";
